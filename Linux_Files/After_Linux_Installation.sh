fa73f086184ba2358b0a30b51ad6f28409c87d79
dinesh-kanth-98 <chdinesh9948@gmail.com>
2023-09-25 21:04:56 +0530

    added practiced jupytor notebook

---
+++ Linux_Files/After_Linux_Installation.sh
@@ -0,0 +1,55 @@
+cd
+
+# sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install vim
+
+sudo apt-get net-tools
+
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer  
+
+sudo apt-get update 
+
+sudo apt-get install grub-customizer -y
+
+sudo apt install vlc
+
+sudo apt install preload
+
+sudo apt install gnome-tweaks
+
+sudo apt  install konsole
+
+sudo apt install dolphin
+
+sudo apt install git && git --version
+
+git config --global user.name "dinesh-kanth-98"
+
+git config --global user.email "chdinesh9948@gmail.com"
+
+mkdir ~/GIT
+
+cd GIT/
+
+git clone https://github.com/dinesh-kanth-98/Dinesh-Kanth.git
+
+cd
+
+cp ~/GIT/Dinesh-Kanth/Linux_Files/bash_aliases ~/.bash_aliases
+
+sudo apt install copyq
+
+
+# installing code
+
+sudo apt install software-properties-common apt-transport-https wget -y
+
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+sudo apt install code && code --version
+
+
+
